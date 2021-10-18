65be9924ef17f129cab5b4a17df3f379b8e8c09b
Roelf de Kock <roelf.dekock@redstor.com>
2021-10-18 06:48:18 +0200

    added canvas drawing

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
+extern crate web_sys;
 mod utils;
 
 use wasm_bindgen::prelude::*;
 use std::fmt::{Display, Formatter, Result as fmtResult};
+
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -19,21 +27,23 @@ extern {
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Cell {
     Dead = 0,
-    Alive = 1
+    Alive = 1,
 }
 
 #[wasm_bindgen]
 pub struct Universe {
     width: u32,
     height: u32,
-    cells: Vec<Cell>
+    cells: Vec<Cell>,
 }
 
 #[wasm_bindgen]
 impl Universe {
     pub fn new() -> Self {
-        let width = 64;
-        let height = 64;
+        let width = 48;
+        let height = 48;
+
+        log!("new universe {} x {}", width, height);
 
         let cells = (0..width * height).map(|i| {
             if i % 2 == 0 || i % 7 == 0 {
@@ -43,10 +53,32 @@ impl Universe {
             }
         }).collect();
 
-        Universe {width, height, cells}
+        Universe { width, height, cells }
     }
 
-    pub fn render(&self) -> String {
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height()).map(|_| Cell::Dead).collect();
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_| Cell::Dead).collect();
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
+    pub fn text_render(&self) -> String {
         self.to_string()
     }
 
@@ -77,10 +109,30 @@ impl Universe {
         (row * self.width + col) as usize
     }
 
+    /*
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() { //why iter().cloned()?
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+    // */
+
+    //*
     fn live_neighbor_count(&self, row: u32, col: u32) -> u8 {
         let mut count: u8 = 0;
         for row_delta in [self.height - 1, 0, 1] {
-            for col_delta in [self.width, 0, 1] {
+            for col_delta in [self.width - 1, 0, 1] {
                 if row_delta == 0 && col_delta == 0 {
                     continue;
                 }
@@ -94,13 +146,33 @@ impl Universe {
 
         count
     }
+    // */
+}
+
+// Another Universe implementation without the wasm_bindgen annotation. This is because Rust-generated
+// WebAssembly functions cannot return borrowed references
+impl Universe {
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
+
 }
 
 impl Display for Universe {
     fn fmt(&self, f: &mut Formatter) -> fmtResult {
         for line in self.cells.as_slice().chunks(self.width as usize) {
             for &cell in line {
-                let symbol = if cell == Cell::Dead {'◻'} else {'◼'};
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
                 write!(f, "{}", symbol);
             }
             write!(f, "\n");
